486e7afa80b2bbb01f837093ecc6875cc048e4a5
Soren Geiger <soerengeiger@gmx.de>
2024-12-11 15:16:13 +0100

    first try of finding the messages quickly

--- main.c
+++ main.c
@@ -3,6 +3,15 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <pthread.h>
+#include <immintrin.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+int g_msg_len = 0;
+int g_start_x = 0;
+int g_start_y = 0;
 
 typedef char i8;
 typedef unsigned char u8;
@@ -12,6 +21,9 @@ typedef unsigned u32;
 typedef unsigned long u64;
 
 #define PRINT_ERROR(cstring) write(STDERR_FILENO, cstring, sizeof(cstring) - 1)
+
+#define true 1
+#define false 0
 
 #pragma pack(1)
 struct bmp_header
@@ -55,8 +67,74 @@ struct file_content   read_entire_file(char* filename)
 	return (struct file_content){file_data, file_size};
 }
 
+int check_header(struct bmp_header* header, u8* pixel_data, u32 x, u32 y)
+{
+	u8* pixel;
+	u32 temp = x;
+	for (u32 i = x; i < 7; i++)
+	{
+		pixel = pixel_data + ((header->height - 1 - y) * header->width + i) * 4;
+		if (pixel[0] != 127 || pixel[1] != 188 || pixel[2] != 217)
+			return (false);
+	}
+	pixel = pixel_data + ((header->height - 1 - y) * header->width + x + 7) * 4;
+	if (pixel[0] == 127 && pixel[1] == 188 && pixel[2] == 217)
+		return (false);
+	else
+	{
+		pixel = pixel_data + ((header->height - 1 - y) * header->width + x + 7) * 4;
+		// printf("found pixel at (%u, %u)\n", x + 7, header->height - 1 - y);
+		// printf("b: %d, r: %d\n", pixel[0], pixel[2]);
+		g_msg_len = pixel[0] + pixel[2];
+		if (g_msg_len > 510)
+			return (false);
+	};
+
+	for (u32 i = y; i < 7; i++)
+	{
+		pixel = pixel_data + ((header->height - 1 - i) * header->width + temp) * 4;
+		if (pixel[0] != 127 || pixel[1] != 188 || pixel[2] != 217)
+			return (false);
+	}
+	g_start_x = x;
+	g_start_y = y;
+	return (true);
+}
+
+void	printing_msg(struct bmp_header* header, u8* pixel_data)
+{
+	int len = g_msg_len;
+	int i = 0;
+	
+	g_start_x += 2;
+	int x = g_start_x;
+	int y = g_start_y + 2;
+
+	char * output = malloc(len + 1);
+	while (i < len)
+	{
+		u8* msg_pixel = pixel_data + ((header->height - 1 - y) * header->width + x) * 4;
+		output[i++] = msg_pixel[0];
+		if (i < len)
+			output[i++] = msg_pixel[1];
+		if (i < len)
+			output[i++] = msg_pixel[2];
+
+		x++;
+		if (g_start_x + 6 == x)
+		{
+			x = g_start_x;
+			y++;
+		}
+	}
+	output[i] = '\0';
+	write (STDOUT_FILENO, output, len);
+	free(output);
+}
+
 int main(int argc, char** argv)
 {
+	// clock_t start_time = clock();
 	if (argc != 2)
 	{
 		PRINT_ERROR("Usage: decode <input_filename>\n");
@@ -69,6 +147,30 @@ int main(int argc, char** argv)
 		return 1;
 	}
 	struct bmp_header* header = (struct bmp_header*) file_content.data;
-	printf("signature: %.2s\nfile_size: %u\ndata_offset: %u\ninfo_header_size: %u\nwidth: %u\nheight: %u\nplanes: %i\nbit_per_px: %i\ncompression_type: %u\ncompression_size: %u\n", header->signature, header->file_size, header->data_offset, header->info_header_size, header->width, header->height, header->number_of_planes, header->bit_per_pixel, header->compression_type, header->compressed_image_size);
-	return 0;
+	// printf("signature: %.2s\nfile_size: %u\ndata_offset: %u\ninfo_header_size: %u\nwidth: %u\nheight: %u\nplanes: %i\nbit_per_px: %i\ncompression_type: %u\ncompression_size: %u\n", header->signature, header->file_size, header->data_offset, header->info_header_size, header->width, header->height, header->number_of_planes, header->bit_per_pixel, header->compression_type, header->compressed_image_size);
+
+	u8* pixel_data = (u8*)(file_content.data + header->data_offset);
+	u8* pixel;
+
+	for (u32 y = 0; y < header->height; y++)
+	{
+		for (u32 x = 0; x < header->width; x++)
+		{
+			pixel = pixel_data + ((header->height - 1 - y) * header->width + x) * 4;
+
+			if (pixel[0] == 127 && pixel[1] == 188 && pixel[2] == 217)
+			{
+				if (check_header(header, pixel_data, x, y) == true)
+				{
+					printing_msg(header, pixel_data);
+					// clock_t end_time = clock();
+					// double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+					// printf("\nElapsed time: %f seconds\n", elapsed_time);
+
+					return (0);
+				}
+			}
+    	}
+	}
+	return (0);
 }
